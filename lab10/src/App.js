573a9cc21e12e35e2136a2f8fbcda067d933647d
sil <git@sil.dev>
2022-06-03 21:43:16 -0500

    lab10 stub

---
+++ lab10/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+{/* TODO: Break up the HTML below into a series of React components. */}
+class App extends React.Component {  
+
+    render () {
+        return (
+            <div>
+
+            <nav className="main-nav">
+                <h1>Photo App</h1>
+                {/* Navigation Links */}
+            </nav>
+
+            <aside>
+                <header>
+                    Profile
+                    {/* Navigation Links */}
+                </header>
+                <div className="suggestions">
+                    <p className="suggestion-text">Suggestions for you</p>
+                    <div>
+                        Suggestions
+                        {/* Suggestions */}
+                    </div>
+                </div>
+            </aside>
+
+            <main className="content">
+                <header className="stories">
+                    Stories
+                    {/* Stories */}
+                </header>
+                <div id="posts">
+                    Posts
+                    {/* Posts */}
+                </div>
+            </main>
+
+            </div>
+        );
+    }
+}
+
+export default App;
